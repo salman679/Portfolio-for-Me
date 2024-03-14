44313b3660ddb660e22af87b7776ef741a5f87f7
Salman Izhar <Sbinazhar671@gmail.com>
2024-03-15 00:22:59 +0600

    init

---
+++ utils/data/contactsData.js
@@ -0,0 +1,12 @@
+export const contactsData = {
+  email: "Sbinazhar671@gmail.com",
+  phone: "+8801690181508",
+  address: "Dogair Bazar, Signboard, Dhaka, Bangladesh",
+  github: "https://github.com/salman679",
+  facebook: "https://www.facebook.com/profile.php?id=100053359325602",
+  linkedIn: "https://www.linkedin.com/in/salman-izhar-37b222221/",
+  twitter: "#",
+  stackOverflow: "#",
+  leetcode: "#",
+  devUsername: "Salman671",
+};
---
+++ utils/data/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "Salman Izhar",
+  profile: "/profile.png",
+  designation: "Software Developer",
+  description:
+    "My name is SALMAN IZHAR. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: "Sbinazhar671@gmail.com",
+  phone: "+8801690181508",
+  address: "Dogair Bazar, Signboard, Dhaka, Bangladesh",
+  github: "https://github.com/salman679",
+  facebook: "https://www.facebook.com/profile.php?id=100053359325602",
+  linkedIn: "https://www.linkedin.com/in/salman-izhar-37b222221/",
+  twitter: "#",
+  stackOverflow: "#",
+  leetcode: "#",
+  devUsername: "Salman671",
+  resume: "#",
+};

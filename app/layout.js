359bd950d54df3265aa76bc41c82479dc19101a3
Salman Izhar <Sbinazhar671@gmail.com>
2025-02-14 00:04:49 +0600

    delete previos profile pic

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Portfolio of Salman Izhar",
   description:
-    "This is the portfolio of Salman Izhar. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Salman Izhar. I am a MERN stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
 };
 
 export default function RootLayout({ children }) {
